dbff58f1f57f97694a314b49df5c3772e0dd5b68
Thiago Coelho <thiag0coelho@outlook.com>
2018-11-07 21:29:22 -0200

    Adicionar arquivos de projeto.

---
+++ RentMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
